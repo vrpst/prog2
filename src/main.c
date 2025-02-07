99223735ad3c1690ee4810fa7a411aaf730f649f
Harrison Baghurst <070806harrison@gmail.com>
2025-02-07 00:22:17 +0000

    added game function structure

--- src/main.c
+++ src/main.c
@@ -2,28 +2,102 @@
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-int main(void) {
+// global variable 
+SDL_Window* window;
+SDL_Surface* screenSurface;
+SDL_Event event;
+bool running = true, upK = false, downK = false, leftK = false, rightK = false; 
+
+// window set up 
+void setWindow() { 
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
+    window = SDL_CreateWindow(
+        "SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1080,
         SDL_WINDOW_FULLSCREEN_DESKTOP
     );
+}
 
-    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+// get inputs  
+void getInputs() {
+    while (SDL_PollEvent(&event)) {
+        switch(event.type) { 
+            case SDL_KEYDOWN: 
+                switch(event.key.keysym.sym) { 
+                    case SDLK_UP: 
+                        upK = true;
+                        break;
+                    case SDLK_DOWN: 
+                        downK = true;
+                        break;
+                    case SDLK_LEFT:
+                        leftK = true;
+                        break;
+                    case SDLK_RIGHT:
+                        rightK = true;
+                        break;
+                }
+            break;
+            case SDL_KEYUP:
+                switch(event.key.keysym.sym) {
+                    case SDLK_UP:
+                        upK = false;
+                        break;
+                    case SDLK_DOWN: 
+                        downK = false;
+                        break;
+                    case SDLK_LEFT:
+                        leftK = false;
+                        break;
+                    case SDLK_RIGHT:
+                        rightK = false;
+                        break;
+                }
+            break;
+            case SDL_QUIT:
+                running = false;
+        }
+    }
+}
+
+
+// update 
+void update() { 
+    if (upK == true) { 
+        printf("%s", "up pressed");
+    }
+    if (downK == true) { 
+        printf("%s", "down pressed");
+    }
+    if (leftK == true) { 
+        printf("%s", "left pressed");
+    }
+    if (rightK == true) { 
+        printf("%s", "right pressed");
+    }
+}
+
+// draw to window 
+void draw() { 
+    screenSurface = SDL_GetWindowSurface(window);
 
     SDL_FillRect(
         screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF)
     );
 
     SDL_UpdateWindowSurface(window);
+}
 
-    SDL_Event e;
-    bool quit = false;
-    while (quit == false) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) quit = true;
-        }
+// game main function 
+int main(void) {
+    
+    setWindow();
+    draw();
+
+    while (running == true) {
+        getInputs();
+        update();
     }
 }
